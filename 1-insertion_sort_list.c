bab7ccae84dad6049ba594ff52fb97239fe2c9e6
Erkenoss <xavier.bertin6@gmail.com>
2023-09-19 13:05:47 +0200

    try fixed case 12 on checker

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *swap_node;
 	listint_t *previous;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
@@ -26,20 +26,17 @@ void insertion_sort_list(listint_t **list)
 		while (swap_node->prev != NULL && swap_node->n < swap_node->prev->n)
 		{
 			previous = swap_node->prev;
-			if (swap_node->n < swap_node->prev->n)
-			{
-				if (swap_node->next != NULL)
-					swap_node->next->prev = previous;
-				if (previous->prev != NULL)
-					previous->prev->next = swap_node;
-				else
-					(*list) = swap_node;
-				previous->next = swap_node->next;
-				swap_node->prev = previous->prev;
-				swap_node->next = previous;
-				previous->prev = swap_node;
-				print_list((*list));
-			}
+			if (swap_node->next != NULL)
+				swap_node->next->prev = previous;
+			if (previous->prev != NULL)
+				previous->prev->next = swap_node;
+			else
+				(*list) = swap_node;
+			previous->next = swap_node->next;
+			swap_node->prev = previous->prev;
+			swap_node->next = previous;
+			previous->prev = swap_node;
+			print_list((*list));
 		}
 		current = current->next;
 	}
