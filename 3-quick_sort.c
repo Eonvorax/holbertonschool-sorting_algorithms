e55b6a94adbd0fba037442fac3dcacfff72e0d3f
Erkenoss <xavier.bertin6@gmail.com>
2023-09-19 11:52:18 +0200

    fixed case when i = j and swap

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -39,8 +39,11 @@ int lomuto_part(int array[], int low, int hight, size_t size)
 		if (array[j] <= pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
 	swap(&array[i + 1], &array[hight]);
