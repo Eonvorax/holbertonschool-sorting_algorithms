e735740dd94333f72c2542236d89fbf93daad740
Erkenoss <xavier.bertin6@gmail.com>
2023-09-19 11:16:14 +0200

    try fixed quicksort task

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -85,6 +85,9 @@ void quick_sort(int *array, size_t size)
 {
 	int low, hight;
 
+	if (array == NULL || size < 2)
+		return;
+
 	low = 0;
 	hight = size - 1;
 	second_quick_sort(array, low, hight, size);
